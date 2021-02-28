97788463c0b987267551283a2752ff0ca6a2f3a4
saraevscreen <saraevscreen@gmail.com>
2021-02-28 19:37:07 +0300

    First commit

---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import s from './Header.module.css'
+
+const Header = () =>{
+    return (
+        <div className={s.header}>
+            <div><img src='https://i.ibb.co/tbVp4Yp/logo0002.png'/></div>
+            <div>NIVERSE.social</div>
+        </div>
+    )
+}
+
+export default Header;
